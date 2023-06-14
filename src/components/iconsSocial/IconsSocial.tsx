2f19b6631bb606379a003474feb54a1d3fda2587
Ruban Pavel <ruban.pavel@outlook.com>
2023-06-14 23:51:03 +0300

    refactor SocialIcons

--- src/components/iconsSocial/IconsSocial.tsx
+++ src/components/iconsSocial/IconsSocial.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import s from './iconsSocial.module.css'
+import git from '../../assets/svgSocial/iconmonstr-github-3.svg'
+import gmail from '../../assets/svgSocial/iconmonstr-gmail-3.svg'
 
 const arrSocialIcons = [
     {
         id: 1,
         href: 'https://github.com/RubanPavel',
-        src: "https://www.svgrepo.com/show/439173/git.svg",
+        src: git,
         alt: 'Git',
     },
     {
         id: 2,
         href: 'https://github.com/RubanPavel',
-        src: "https://www.svgrepo.com/show/439173/git.svg",
-        alt: 'Git',
+        src: gmail,
+        alt: 'Gmail',
     },
     {
         id: 3,
